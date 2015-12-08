c90599b3ae94917e40f82edb7df7b46ec15f2b91
golohas <985792686@qq.com>
2015-12-08 18:06:02 +0800

    Create ConfigParserTest.py

---
+++ ConfigParserTest.py
@@ -0,0 +1,124 @@
+# -*- coding:utf-8 -*-
+__author__ = 'golohas'
+'''
+#filename: ConfigParserTest.py
+#python can read and write ini file by import ConfigParser
+----------------------------
+#2015-12-08 created
+----------------------------
+Configuration file parser.
+
+A setup file consists of sections, lead by a "[section]" header,
+and followed by "name: value" entries, with continuations and such in
+the style of RFC 822.
+
+The option values can contain format strings which refer to other values in
+the same section, or values in a special [DEFAULT] section.
+
+For example:
+
+    something: %(dir)s/whatever
+
+would resolve the "%(dir)s" to the value of dir.  All reference
+expansions are done late, on demand.
+
+Intrinsic defaults can be specified by passing them into the
+ConfigParser constructor as a dictionary.
+
+class:
+
+ConfigParser -- responsible for parsing a list of
+                configuration files, and managing the parsed database.
+
+    methods:
+
+    __init__(defaults=None)
+        create the parser and specify a dictionary of intrinsic defaults.  The
+        keys must be strings, the values must be appropriate for %()s string
+        interpolation.  Note that `__name__' is always an intrinsic default;
+        its value is the section's name.
+
+    sections()
+        return all the configuration section names, sans DEFAULT
+
+    has_section(section)
+        return whether the given section exists
+
+    has_option(section, option)
+        return whether the given option exists in the given section
+
+    options(section)
+        return list of configuration options for the named section
+
+    read(filenames)
+        read and parse the list of named configuration files, given by
+        name.  A single filename is also allowed.  Non-existing files
+        are ignored.  Return list of successfully read files.
+
+    readfp(fp, filename=None)
+        read and parse one configuration file, given as a file object.
+        The filename defaults to fp.name; it is only used in error
+        messages (if fp has no `name' attribute, the string `<???>' is used).
+
+    get(section, option, raw=False, vars=None)
+        return a string value for the named option.  All % interpolations are
+        expanded in the return values, based on the defaults passed into the
+        constructor and the DEFAULT section.  Additional substitutions may be
+        provided using the `vars' argument, which must be a dictionary whose
+        contents override any pre-existing defaults.
+
+    getint(section, options)
+        like get(), but convert value to an integer
+
+    getfloat(section, options)
+        like get(), but convert value to a float
+
+    getboolean(section, options)
+        like get(), but convert value to a boolean (currently case
+        insensitively defined as 0, false, no, off for False, and 1, true,
+        yes, on for True).  Returns False or True.
+
+    items(section, raw=False, vars=None)
+        return a list of tuples with (name, value) for each option
+        in the section.
+
+    remove_section(section)
+        remove the given file section and all its options
+
+    remove_option(section, option)
+        remove the given option from the given section
+
+    set(section, option, value)
+        set the given option
+
+    write(fp)
+        write the configuration state in .ini format
+'''
+
+import ConfigParser
+
+def test(config_file_path):
+	#init cf and read config_file
+	cf = ConfigParser.ConfigParser()
+	cf.read(config_file_path)
+
+	s = cf.sections()
+	print 'sections: ', s
+
+	o = cf.options("baseconf")
+	print 'options: ', o
+
+	v = cf.items("baseconf")
+	print 'db: ', v
+
+	db_host = cf.get('baseconf', 'host')
+	db_port = cf.getint('baseconf', 'port')
+	db_user = cf.get('baseconf', 'user')
+	db_pwd = cf.get('baseconf', 'passwd')
+	print db_host, db_port, db_user, db_pwd
+
+	cf.set('baseconf', 'db_passwd', '123456')
+	cf.write(open('config_file_path', 'w'))
+
+if __name__ == "__main__":
+	test("db_config.ini")
